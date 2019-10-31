8611283f36e0add2d4042aa42df5f964179cb9b7
tothricsaj <tothricsaj@gmail.com>
2019-10-31 16:28:01 +0100

    actions is added

--- src/app/app.js
+++ src/app/app.js
@@ -1,3 +1,3 @@
 export default function Main() {
-    document.body.insertAdjacentHTML('afterbegin', '<h2 style="color: darkorange; border-bottom: 1px solid black; text-align: center;">Hello Redux</h2>');
+    console.log('%cHello Redux!!!', 'color: orange; border:1px solid orange; padding: 5px');
 }
