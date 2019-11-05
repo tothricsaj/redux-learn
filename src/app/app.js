dc17ea8a393cba945d9a56afb8e521def45cf128
tothricsaj <tothricsaj@gmail.com>
2019-11-05 12:48:24 +0100

    basic storing method

--- src/app/app.js
+++ src/app/app.js
@@ -1,3 +1,13 @@
+import { createStore} from "redux";
+import { getInputAction } from "./actions";
+import { getInputReducer } from "./reducers";
+
 export default function Main() {
     console.log('%cHello Redux!!!', 'color: orange; border:1px solid orange; padding: 5px');
+
+    let store = createStore(getInputReducer);
+
+    store.dispatch(getInputAction('Kis cica fasza!!!'));
+
+    console.log(`%c${store.getState()}`, 'color: orangered; border:1px solid orangered; padding: 5px');
 }
--- src/app/reducers.js
+++ src/app/reducers.js
@@ -1,2 +1,10 @@
-import { combineReducers } from 'redux'
+import { GET_INPUT } from "./actions";
 
+export const getInputReducer = (state='Zero input', action) => {
+    switch (action.type) {
+        case GET_INPUT:
+            return state = action.input;
+        default:
+            return state;
+    }
+};
